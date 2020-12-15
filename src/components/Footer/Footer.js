1ac3bbd1906da84a691bc28f1642995cb6bcc3cd
Dorota <d.a.lewinska@gmail.com>
2020-12-15 22:19:17 +0000

    Styling added to header and footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,9 @@
+import './Footer.css';
 function Footer() {
     return (
       <footer>
       <div className="container">
-        <span>Place footer content here.</span>
+        <div>Place footer content here.</div>
       </div>
     </footer>
     );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,16 @@
 import './Header.css';
 import {
   BrowserRouter as Router,
+  Link,
   NavLink
 } from "react-router-dom";
 function Header() {
     return (
-  
         <header>
         <nav className="navbar">
           <div className="container">
-          <a className="navbar-brand" href="/">Let's Recycle</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse " id="navbarCollapse">
-            <ul className="navbar-nav ml-auto ">
+            <Link to="/" >&#9851; <span>Let's Recycle</span></Link>
+            <ul className="navbar-nav">
               <li className="nav-item">
                 <NavLink to="/" activeClassName="active">Home</NavLink>
               </li>
@@ -25,7 +21,6 @@ function Header() {
                 <NavLink to="/map" activeClassName="active">Map</NavLink>
               </li>
             </ul>
-          </div>
           </div>
     
         </nav>
