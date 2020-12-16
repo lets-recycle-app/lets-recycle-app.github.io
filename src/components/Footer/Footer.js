133a8e0a32bbc23187bfdd877304d08df01685ba
Dorota <d.a.lewinska@gmail.com>
2020-12-16 17:34:00 +0000

    Restoring from deployment that went wrong.

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.css';
+function Footer() {
+    return (
+      <footer>
+      <div className="container">
+        <div>Place footer content here.</div>
+      </div>
+    </footer>
+    );
+}
+
+export default Footer;
