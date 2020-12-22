09c321ea8e5b9ed31850675d6f2fa409982a834d
Dorota <d.a.lewinska@gmail.com>
2020-12-22 19:23:14 +0000

    Form validation nearly completed (postcode still to do).

--- src/components/FormReport/FormReport.js
+++ src/components/FormReport/FormReport.js
@@ -1,34 +1,94 @@
 import "./FormReport.css";
 import {useState} from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+//console.log(uuidv4());
 
 function FormReport() {
-  const [locationType, setLocationType] = useState("");
-  const [inputName, setInputName] = useState("");
-  const [inputEmail, setInputEmail] = useState("");
-  const [inputAppliance, setInputAppliance] = useState("");
-  const [inputHouseNo, setInputHouseNo] = useState("");
-  const [inputStreet, setInputStreet] = useState("");
-  const [inputTown, setInputTown] = useState("");
-  const [inputPostcode, setInputPostcode] = useState("");
-  const [inputNotes, setInputNotes] = useState("");
+  const [locationType, setLocationType] = useState({value:""});
+  const [inputName, setInputName] = useState({value:""});
+  const [inputEmail, setInputEmail] = useState({value:""});
+  const [inputAppliance, setInputAppliance] = useState({value:""});
+  const [inputHouseNo, setInputHouseNo] = useState({value:""});
+  const [inputStreet, setInputStreet] = useState({value:""});
+  const [inputTown, setInputTown] = useState({value:""});
+  const [inputPostcode, setInputPostcode] = useState({value:""});
+  const [inputNotes, setInputNotes] = useState({value:""});
+  const [messageOnSubmit, setMessageOnSubmit] = useState([]);
 
+  /* do this on submission??? */
   const handleRadioPublic = e => {
     setLocationType(e.target.value);
-    setInputName("");
-    setInputEmail("");
+    setInputName({value:""});
+    setInputEmail({value:""});
+  }
+
+  const validateForm = e => {
+    let errorMsg = [];
+    //console.log(locationType, inputName, inputEmail, inputAppliance, inputHouseNo,inputStreet, inputTown, inputPostcode, inputNotes);
+    if(locationType.value === ""){
+      errorMsg.push("Please select Location Type");
+      setLocationType({value: "", css: "textRed"});
+    }
+    if(inputName.value === ""){
+      errorMsg.push("Please enter your forename and surname.");
+      setInputName({value: "", css: "borderRed"});
+      //console.log('lorem', inputName);
+    }
+
+    var validator = require("email-validator");
+    if(inputEmail.value === "" || validator.validate(inputEmail.value) !== true){
+      errorMsg.push("Please enter a valid email.");
+      setInputEmail({value: inputEmail.value, css: "borderRed"});
+    }
+    if(inputAppliance.value === ""){
+      errorMsg.push("Please select appliance type.");
+      setInputAppliance({value: "", css: "borderRed"});
+    } 
+    if(inputHouseNo.value === ""){
+      errorMsg.push("Please enter house/building number.");
+      setInputHouseNo({value: "", css: "borderRed"});
+    }
+    if(inputStreet.value === ""){
+      errorMsg.push("Please enter street.");
+      setInputStreet({value: "", css: "borderRed"});
+    }
+    if(inputTown.value === ""){
+      errorMsg.push("Please enter town or city.");
+      setInputTown({value: "", css: "borderRed"});
+    } 
+    if(inputPostcode.value === ""){
+      errorMsg.push("Please enter postcode.");
+      setInputPostcode({value: "", css: "borderRed"});
+    }     
+    //console.log(errorMsg);
+    return errorMsg;
   }
 
   const submitForm = e => {
-    
-    //console.log(locationType, inputName, inputEmail, inputAppliance, inputHouseNo,inputStreet, inputTown, inputPostcode, inputNotes);
 
+    e.preventDefault();
+    let validation = validateForm(e);
+    //there was an error
+    if(validation.length > 0){
+        setMessageOnSubmit(( validation ));
+      
+    } else {
+      console.log('success');  
+    }
+
+   //console.log(validation);
   }
 
-    return (
-      <form>
+//console.log('lorem', messageOnSubmit);
+    return (    
+      <form onSubmit={ submitForm }>
+        <div className="errorMsg">
+        { messageOnSubmit.map((line, i) => <MsgLine line={line} key={i} />)}
+        </div>
         <div className="form-row">
           <label>Type of location</label>
-          <div>
+          <div className={ locationType.css } >
             <div className="form-check-inline">
               <input 
               type="radio" 
@@ -36,7 +96,7 @@ function FormReport() {
               id="inlineRadio1"
               onChange={ handleRadioPublic }  
               value="public area" 
-              checked = {locationType === "public area" }
+              checked = {locationType.value === "public area" }
               />
               <label htmlFor="inlineRadio1">Public Area</label>
             </div>
@@ -46,14 +106,14 @@ function FormReport() {
               name="locationType" 
               id="inlineRadio2" 
               value="private property" 
-              onChange={ e => setLocationType(e.target.value) }  
-              checked={ locationType === "private property" }
+              onChange={ e => setLocationType({value: e.target.value}) }  
+              checked={ locationType.value === "private property" }
               />
               <label htmlFor="inlineRadio2">Private Property </label>
             </div>
           </div>
         </div>
-        <div hidden={locationType === "private property" ? "" : "hidden"} >
+        <div hidden={locationType.value === "private property" ? "" : "hidden"} >
           <div className="form-row">
             <label htmlFor="inputName">Full Name</label>
             <div>
@@ -61,8 +121,9 @@ function FormReport() {
               type="text" 
               id="inputName"
               name="inputName"
-              onChange = { e => setInputName(e.target.value) }
-              value = { inputName } 
+              onChange = { e => setInputName({value: e.target.value}) }
+              value = { inputName.value } 
+              className={ inputName.css }
               />
             </div>
           </div>
@@ -73,8 +134,9 @@ function FormReport() {
               type="text" 
               id="inputEmail" 
               name="inputEmail"
-              onChange = { e => setInputEmail(e.target.value) }
-              value = { inputEmail }
+              onChange = { e => setInputEmail({value: e.target.value}) }
+              value = { inputEmail.value }
+              className = { inputEmail.css }
               />
             </div>
           </div>
@@ -85,7 +147,8 @@ function FormReport() {
             <select 
             id="inputAppliance" 
             name="appliance"
-            value={inputAppliance} 
+            value={inputAppliance.value} 
+            className={ inputAppliance.css }
             onChange={e => setInputAppliance(e.target.value)}
             >
               <option value="">Select...</option>
@@ -95,12 +158,13 @@ function FormReport() {
           </div>
         </div>
         <div className="form-row">
-          <label htmlFor="inputHouseNo">House No</label>
+          <label htmlFor="inputHouseNo">House/Building No</label>
           <div>
             <input 
             type="text"
             id="inputHouseNo"
-            value={ inputHouseNo }
+            value={ inputHouseNo.value }
+            className={ inputHouseNo.css }
             onChange={ e => setInputHouseNo(e.target.value) }
             />
           </div>
@@ -111,7 +175,8 @@ function FormReport() {
             <input 
             type="text"
             id="inputStreet" 
-            value={ inputStreet }
+            value={ inputStreet.value }
+            className={ inputStreet.css }
             onChange={ e => setInputStreet(e.target.value) }
             />
           </div>
@@ -122,7 +187,8 @@ function FormReport() {
             <input 
             type="text"
             id="inputTown" 
-            value={ inputTown }
+            value={ inputTown.value }
+            className={ inputTown.css }
             onChange={ e => setInputTown(e.target.value) }
             />
           </div>
@@ -133,7 +199,8 @@ function FormReport() {
             <input 
             type="text" 
             id="inputPostcode" 
-            value={ inputPostcode }
+            value={ inputPostcode.value }
+            className={ inputPostcode.css }
             onChange={ e => setInputPostcode(e.target.value) }
             />
           </div>
@@ -146,16 +213,24 @@ function FormReport() {
             <textarea 
             id="inputNotes"
             name="inputNotes"
-            value={ inputNotes }
+            value={ inputNotes.value }
+            className={ inputNotes.css }
             onChange={ e => setInputNotes(e.target.value) }
             ></textarea>
           </div>
         </div>
         <div className="form-row text-right">
-            <button type="submit" onClick={ submitForm } >Submit</button>
+            <button type="submit">Submit</button>
         </div>      
       </form>
     );
+}
+function MsgLine(props) {
+  //console.log('aaa', props.msgArray, props.css);
+  return(   
+      <span>{ props.line }</span>
+  );
+
 }
 
 export default FormReport;
