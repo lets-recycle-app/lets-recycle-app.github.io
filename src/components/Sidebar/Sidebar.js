f2d373c8ffb1551c5ce99c30d8716d93ee801524
Dorota <d.a.lewinska@gmail.com>
2021-01-06 20:06:04 +0000

    Minor amend to sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -7,17 +7,17 @@ function Sidebar() {
     return (
         <div className="side-column">
             <p>
-                <a href="https://ao.com" target="_blank">
+                <a href="https://ao.com" target="_blank" rel="noreferrer">
                     <img src={ad1} alt="advertisement1" />
                 </a>
             </p>
             <p>
-                <a href="https://ao.com" target="_blank">
+                <a href="https://ao.com" target="_blank" rel="noreferrer">
                     <img src={ad2} alt="advertisement2" />
                 </a>
             </p>
             <p>
-                <a href="https://ao.com" target="_blank">
+                <a href="https://ao.com" target="_blank" rel="noreferrer">
                     <img src={ad3} alt="advertisement3" />
                 </a>
             </p>
