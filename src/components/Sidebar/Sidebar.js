518a580ebc59067c06c7a073190a948c9a8c99a9
Dorota <d.a.lewinska@gmail.com>
2021-02-09 20:14:47 +0000

    Ads amended

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ad1 from '../../images/aoLapTopAd.png';
-import ad2 from '../../images/aoBusAd.png';
-import ad3 from '../../images/aoWashAd.png';
+import ad1 from '../../images/aoLapTopAd1.png';
+import ad2 from '../../images/aoBusAd1.png';
+import ad3 from '../../images/aoWashAd1.png';
 import './Sidebar.css';
 
 function Sidebar() {
@@ -22,7 +22,21 @@ function Sidebar() {
                     <img src={ad3} alt="advertisement3" />
                 </a>
             </p>
-
+            <p>
+                <a href="https://ao.com" target="_blank" rel="noreferrer">
+                    <img src={ad1} alt="advertisement1" />
+                </a>
+            </p>
+            <p>
+                <a href="https://ao.com" target="_blank" rel="noreferrer">
+                    <img src={ad2} alt="advertisement2" />
+                </a>
+            </p>
+            <p>
+                <a href="https://ao.com" target="_blank" rel="noreferrer">
+                    <img src={ad3} alt="advertisement3" />
+                </a>
+            </p>
         </div>
   );
 }
